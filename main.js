8e59e6f50e9dab6e7ef48450c436524b016f19be
tarorinn <72378248+tarorinn@users.noreply.github.com>
2021-03-17 16:09:24 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+{
+  const targets = document.querySelectorAll('img');
+
+  function callback(entries, obs) {
+    console.log(entries);
+
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+  
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+    // threshold: 1,
+    rootMargin: '0px 0px -170px',
+  }
+
+  const observer = new IntersectionObserver(callback, options);
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+}
